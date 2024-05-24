0ef839ec716de1da553230c9da1be72f6d9fe41a
yu1210 <104638197+nikolaospaterakis@users.noreply.github.com>
2024-05-24 18:45:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const body = document.getElementById("body")
 const loader = document.getElementById("loader")
 const main = document.getElementById("main")
+const width = window.innerWidth;
+
 setTimeout(() => {
     hideLoader()
    showMainContent()
@@ -11,8 +13,31 @@ function hideLoader(){
     loader.style.opacity = "0"
 }
 
+window.addEventListener("resize", () => {
+    console.log("hey")
+    const width = window.innerWidth;
+    if(width < 1279) {
+        body.style.backgroundImage = "none"
+    } else {
+            body.style.backgroundImage = `url("/images/bg.png")`
+            body.style.backgroundRepeat = "no-repeat"
+            body.style.backgroundAttachment = "fixed"
+            body.style.backgroundPosition = "right bottom"
+            body.style.backgroundSize = "46% 89%"
+    }
+})
+
 function showMainContent(){
     setTimeout(() => {
         main.style.opacity = "100"
+        if(width > 1279) {
+            body.style.backgroundImage = `url("/images/bg.png")`
+            body.style.backgroundRepeat = "no-repeat"
+            body.style.backgroundAttachment = "fixed"
+            body.style.backgroundPosition = "right bottom"
+            body.style.backgroundSize = "46% 89%"
+        }
+        
     }, 1000);
 }
+
